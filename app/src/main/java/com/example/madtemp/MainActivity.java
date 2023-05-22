2f1c587bf90cb57e9713946b41b555f0d289ca74
ayd3n21 <jam.ayden05@gmail.com>
2023-05-22 11:46:53 +0800

    pushing

--- app/src/main/java/com/example/madtemp/MainActivity.java
+++ app/src/main/java/com/example/madtemp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.madtemp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,37 +10,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    User person = new User("John","description",1739039545,false);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent rec = getIntent();
+        String name = rec.getStringExtra("name");
+        String des = rec.getStringExtra("des");
+        String fn = rec.getStringExtra("fnum");
         TextView txt = (TextView) findViewById(R.id.textView);
-        txt.setText(person.getName() + " " + person.getId());
+        txt.setText(name);
+        TextView description = (TextView) findViewById(R.id.textView2);
+        description.setText("Description: " + des);
         Button btn = (Button) findViewById(R.id.button);
-        if(person.getFollowed() == false){
+        if (fn == "0") {
             btn.setText("FOLLOW");
-        }
-        else{
+        } else {
             btn.setText("UNFOLLOW");
         }
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener(){
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(btn.getText() == "UNFOLLOW"){
+                if (btn.getText() == "UNFOLLOW") {
                     btn.setText("FOLLOW");
-                    Toast.makeText(getApplicationContext(),"Unfollowed",Toast.LENGTH_SHORT).show();
-                    person.setFollowed(true);
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
+
                 } else if (btn.getText() == "FOLLOW") {
                     btn.setText("UNFOLLOW");
-                    Toast.makeText(getApplicationContext(),"Followed",Toast.LENGTH_SHORT).show();
-                    person.setFollowed(false);
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
+
                 }
             }
         });
